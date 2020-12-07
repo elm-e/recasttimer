dba748aec15e3f5f2f960211d85bab304f860894
elm-e <sadal.suud.2125@gmail.com>
2020-12-07 23:40:14 +0900

    ソート機能追加

--- main.js
+++ main.js
@@ -10,6 +10,13 @@ var myName = '';
 
 var storage = localStorage;
 var config = JSON.parse(storage.getItem('recasttimer_config'));
+
+var order = {
+    name: 1,
+    skill: 1,
+    lock: 1,
+}
+
 if (config===null) {
     console.log('Config init');
     config = {
@@ -56,45 +63,46 @@ function setValue(i, value) {
     $("#recast-table tbody tr").eq(i).find('.recast').text(value);
 }
 
-function load(callback) {
-    $('#recast-table tbody').html('');
+function setTable() {
+    recastData = {};
+    for (let i in recastList) {
+        let key = `${recastList[i].name}.${recastList[i].skill}`;
+        let hidden = config.hidden.indexOf(key) >= 0;
+        let locked = config.locked.indexOf(key) >= 0;
+        
+        recastData[key] = i;
+        recastList[i].value = 'Ready';
+        recastList[i].lasttime = null;
 
+        let tr = $('<tr></tr>');
+        $(tr).append(`<td class="name"><span>${recastList[i].name}</span></td>`);
+        $(tr).append(`<td class="skill"><span>${recastList[i].skill}</span></td>`);
+        $(tr).append(`<td class="view"><span><input type="checkbox" id="${key}"></input><label for="${key}" class="checkbox"></label></span></td>`);
+        $(tr).append(`<td class="recast" style="color:#e3ea7d">${recastList[i].value}</td>`);
+
+        if (hidden) {
+            $(tr).hide();
+            setFilter(hidden);
+        }
+
+        if (locked)
+            $(tr).find('input[type=checkbox]').prop('checked', true);
+
+        $('#recast-table tbody').append($(tr));
+    }
+}
+
+function load(callback) {
+    $('#recast-table tbody').empty();
     $.ajax({
         type: 'GET',
         url: 'https://script.google.com/macros/s/AKfycbzNYd8qbaDaUYtOo_cSSh9SKYZTdwrZaago4gm_npS38PcKRYo/exec',
         dataType: 'json',
     }).done(function (result) {
         recastList = result;
-        for (let i in recastList) {
-            let key = `${recastList[i].name}.${recastList[i].skill}`;
-            let hidden = config.hidden.indexOf(key) >= 0;
-            let locked = config.locked.indexOf(key) >= 0;
-            
-            recastData[key] = i;
-            recastList[i].value = 'Ready';
-            recastList[i].lasttime = null;
-    
-            let tr = $('<tr></tr>');
-            $(tr).append(`<td class="name"><span>${recastList[i].name}</span></td>`);
-            $(tr).append(`<td class="skill"><span>${recastList[i].skill}</span></td>`);
-            $(tr).append(`<td class="view"><span><input type="checkbox" id="${key}"></input><label for="${key}" class="checkbox"></label></span></td>`);
-            $(tr).append(`<td class="recast" style="color:#e3ea7d">${recastList[i].value}</td>`);
-
-            if (hidden) {
-                $(tr).hide();
-                setFilter(hidden);
-            }
-
-            if (locked) {
-                $(tr).find('input[type=checkbox]').prop('checked', true);
-            }
-
-            $('#recast-table tbody').append($(tr));
-        }
-    
-        console.log('recastList', recastList);
-        console.log('recastData', recastData);
-        
+        setTable();
+        // console.log('recastList', recastList);
+        // console.log('recastData', recastData);
         if (callback)
             callback()
 
@@ -214,6 +222,43 @@ $(function () {
         saveConfig();
     });
     
+    $('#recast-table thead th.sort').on('click', function() {
+        $('#recast-table tbody').empty();
+        var target = $(this).attr('name');
+        var orderStr = '';
+
+        if (target !== 'lock') {
+            recastList.sort((a, b) => {
+                if (a[target] < b[target]) return -1 * order[target];
+                if (a[target] > b[target]) return 1 * order[target];
+                return 0;
+            });
+        }
+        else {
+            recastList.sort((a, b) => {
+                let akey = `${a.name}.${a.skill}`;
+                let bkey = `${b.name}.${b.skill}`;
+                let alocked = config.locked.indexOf(akey) >= 0;
+                let blocked = config.locked.indexOf(bkey) >= 0;
+                if (alocked && !blocked) return -1 * order[target];
+                if (!alocked && blocked) return 1 * order[target];
+                return 0;
+            });
+        }
+
+        if (order[target] === 1) {
+            orderStr = '↑';
+            $(this).find('span').css('color', 'cyan');
+        }
+        else {
+            orderStr = '↓';
+            $(this).find('span').css('color', 'darkorange');
+        }
+        $(this).find('span').text(orderStr);
+        order[target] *= -1;
+        setTable();
+    });
+
     $('#recast-table').on('click', 'td.skill span', function() {
         var skill = $(this).text();
         $('#recast-table tbody').find('tr').each(function(i, elem) {
